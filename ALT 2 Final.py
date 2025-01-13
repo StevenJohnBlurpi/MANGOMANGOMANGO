f6e808880d8543f565a69127a9fe583bac1b2af7
StevenJohnBlurpi <stevenjohnblurpi@gmail.com>
2025-01-13 14:52:21 +0000

    Add files via upload

--- ALT 2 Final.py
+++ ALT 2 Final.py
@@ -10,6 +10,29 @@ with open("Death Polish.txt", "r", encoding = "utf-8") as Death_Polish_Text:
 with open("Death Chinese.txt", "r", encoding = "utf-8") as Death_Chinese_Text:
     death_read_text_chinese = Death_Chinese_Text.read()
 
-
 death_read_text_english = death_read_text_english.split(" ")
 print(death_read_text_english)
+
+a = 0
+e = 0
+i = 0
+o = 0
+u = 0
+        
+for vowel in death_read_text_english:
+    if vowel == "a":
+        a += 1
+    elif vowel == "e":
+        e += 1
+    elif vowel == "i":
+        i += 1
+    elif vowel == "o":
+        o += 1
+    elif vowel == "u":
+        u += 1
+
+print(a)
+print(e)
+print(i)
+print(o)
+print(u)
