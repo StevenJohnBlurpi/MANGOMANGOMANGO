a21a31371ec38b961f955fb3bc0cc2e6fc8218dd
StevenJohnBlurpi <stevenjohnblurpi@gmail.com>
2025-01-15 12:16:40 +0000

    Add files via upload

--- ALT 2 Final - The GOAT.py
+++ ALT 2 Final - The GOAT.py
@@ -13,7 +13,7 @@ with open("Death Chinese.txt", "r", encoding = "utf-8") as Death_Chinese_Text:
 with open("kjv.txt", "r", encoding = "utf-8") as KJV_Text:
     kjv_read = KJV_Text.read()
 
-first_text = input("Which text would you like to compare with? Choices are: Death, KJV: English Only")
+first_text = input("Which text would you like to compare with? Choices are: Death, KJV: English Only. ")
 
 if first_text == "Death":
     first_text = "Death"
@@ -23,35 +23,26 @@ else:
     print("Invalid Input")
 
 if first_text == "Death":
-    first_langauge = input("Which language would you like to compare with? Choices are: English, Latvian, Polish and Chinese. ")
-elif first_text =="KJV":
+    first_langauge = input("Which language would you like to compare the first text with? Choices are: English, Latvian, Polish and Chinese. ")
+elif first_text == "KJV":
     first_language = kjv_read
 else:
     print("Invalid Input")
-    
-    
-    
 
-
-
-
-
-
-
-
-first_language = input("Which language would you like to compare with? Choices are: English, Latvian, Polish and Chinese. ")
+first_language = ""
 
 if first_language == "English":
-    first_language = "English"
+    first_language = death_read_text_english
 elif first_language == "Latvian":
-    first_language = "Latvian"
+    first_language = death_read_text_latvian
 elif first_language == "Polish":
-    first_language = "Polish"
-elif first_language =="Chinese":
-    first_language = "Chinese"
+    first_language = death_read_text_polish
+elif first_language == "Chinese":
+    first_language = death_read_text_chinese
 else:
     print("Invalid Input")
 
+print(first_language)
 
 a = 0
 e = 0
@@ -70,9 +61,3 @@ for vowel in death_read_text_english:
         o += 1
     elif vowel == "u":
         u += 1
-
-print(a)
-print(e)
-print(i)
-print(o)
-print(u)
